e07ef187058750f0e70715da882d2b8fc5348f8d
BobbyESP <gabilessto@gmail.com>
2020-08-19 15:13:06 +0200

    1

--- app/src/main/java/com/bobby/valomanager/MainActivity.java
+++ app/src/main/java/com/bobby/valomanager/MainActivity.java
@@ -2,13 +2,35 @@ package com.bobby.valomanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        videoView =(VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.vid1);
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if (isFinishing())
+                    return;
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+                finish();
+
+            }
+        });
+        videoView.start();
+
     }
 }
