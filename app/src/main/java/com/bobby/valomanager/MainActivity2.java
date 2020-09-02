0b3aa4500d3d9aee675114ddbde4374435ec43b0
BobbyESP <gabilessto@gmail.com>
2020-09-02 17:19:55 +0200

    1

--- app/src/main/java/com/bobby/valomanager/MainActivity2.java
+++ app/src/main/java/com/bobby/valomanager/MainActivity2.java
@@ -2,13 +2,29 @@ package com.bobby.valomanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        boton = (Button) findViewById(R.id.boton_prueba);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+                startActivity(new Intent(MainActivity2.this, MainActivity.class));
+                overridePendingTransition(0, 0);
+            }
+        });
     }
 }
+
