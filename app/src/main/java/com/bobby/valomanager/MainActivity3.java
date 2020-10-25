f01feb93a3fc64cc13c31e37877dcdb3c8ab1ddd
BobbyESP <gabilessto@gmail.com>
2020-10-25 17:34:47 +0100

    1

--- app/src/main/java/com/bobby/valomanager/MainActivity3.java
+++ app/src/main/java/com/bobby/valomanager/MainActivity3.java
@@ -1,9 +1,14 @@
 package com.bobby.valomanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity3 extends AppCompatActivity {
     TextView txt_animation;
@@ -12,5 +17,31 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        BottomNavigationView bottomNav = findViewById(R.id.principal_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new InicioFragment()).commit();
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new InicioFragment();
+                            break;
+                        case R.id.Desafíos:
+                            selectedFragment = new DesafiosFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
