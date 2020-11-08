a92b6ad39431db2dec8b1a67ed99413b2cc47f0f
BobbyESP <gabilessto@gmail.com>
2020-11-08 12:06:31 +0100

    1

--- app/src/main/java/com/bobby/valomanager/Fragments/APITESTFragment.java
+++ app/src/main/java/com/bobby/valomanager/Fragments/APITESTFragment.java
@@ -12,9 +12,12 @@ import androidx.fragment.app.Fragment;
 import com.bobby.valomanager.R;
 
 public class APITESTFragment extends Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_apitest, container, false);
     }
+
 }
